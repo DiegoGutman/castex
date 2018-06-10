8bb44801bf819a9b1435382ae4b942606a5ecd60
Diego Gutman <diego.gutman85@gmail.com>
2018-06-10 13:06:00 -0300

    agrego favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="shortcut icon" href="{{{ public_path('favicon.png') }}}">
 </head>
 <body>
     <div id="app">
