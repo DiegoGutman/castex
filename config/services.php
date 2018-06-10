5b95b0cef9c017903533b7912ca360235bda152f
Diego Gutman <diego.gutman85@gmail.com>
2018-06-10 13:10:10 -0300

    se pasa facebook callback a env

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),         // Your Facebook Client ID
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'), // Your Facebook Client Secret
-        'redirect' => 'https://castex.vm/login/facebook/callback'
+        'redirect' => env('FACEBOOK_CLIENT_CALLBACK')
     ],
 
 ];
